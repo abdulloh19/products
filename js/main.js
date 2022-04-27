2b02937c3ae6d62247507650f5a5d21e56a66fea
abdulloh19 <parij4320@gmail.com>
2022-04-28 00:17:50 +0500

    for bilan ishlandi

--- js/main.js
+++ js/main.js
@@ -1,35 +1,63 @@
-const elProductWrapper = document.querySelector("ul");
+const elProductWrapper = document.querySelector(".product-wrapper");
+elProductWrapper.className = "row list-unstyled g-3";
 
-const elProduct = document.createElement("li");
-elProduct.className = "col-4";
-
-const elProductWrap = document.createElement("div");
-elProductWrap.className = "card";
-const elProductImg = document.createElement("img");
-elProductImg.src = product.img;
-elProductImg.className = "card-img-top";
-const elPoroductBody = document.createElement("div");
-elPoroductBody.className = "card-body";
-const elProductTitle = document.createElement("h3");
-elProductTitle.textContent = product.title;
-elProductTitle.className = "card-title";
-const elProductPrice = document.createElement("p");
-elProductPrice.className = "card-text fw-bold";
-const elProductMark = document.createElement("mark");
-elProductMark.textContent = product.price;  
-const elProductModel = document.createElement("p");
-elProductModel.textContent = product.model;
-elProductModel.className = "badge bg-success";
+// const producs = {
+//     id: 123,
+//     title: "Redmi Note 10 Pro",
+//     img: "https://picsum.photos/300/200",
+//     price: 4300000,
+//     model: "Xiaomi",
+//     addedDate: new Date("2021-11-12").toISOString(),
+//     benefits: ["8gb", "128gb", "Waterproof"]
+//   };
 
 
-elProduct.append(elProductWrap);
-elProductWrap.append(elProductImg);
-elProductWrap.append(elPoroductBody);
-elPoroductBody.append(elProductTitle)
-elPoroductBody.append(elProductPrice);
-elProductPrice.append(elProductMark);
-elPoroductBody.append(elProductModel);
 
-console.log(product);
+for (let i = 0; i < products.length; i++) {
+    const elProducts = document.createElement("li");
+    elProducts.className = "col-4";
+    const elProductItem = document.createElement("div");
+    elProductItem.className = "card";
+    const elProductImg = document.createElement("img");
+    elProductImg.src = products[i].img;
+    elProductImg.className = "card-img-top";
+    const elPoroductBody = document.createElement("div");
+    elPoroductBody.className = "card-body";
+    const elProductTitle = document.createElement("p");
+    elProductTitle.textContent = products[i].title;
+    elProductTitle.className = "card-title";
+    const elProductPrice = document.createElement("p");
+    elProductPrice.className = "card-text fw-bold";
+    const elProductMark = document.createElement("mark");
+    elProductMark.textContent = products[i].price;  
+    const elProductModel = document.createElement("p");
+    elProductModel.textContent = products[i].model;
+    elProductModel.className = "badge bg-success";
+    const elProductIcons = document.createElement("div");
+    elProductIcons.className = "position-absolute top-0 end-0 d-flex";
+    const elPoroductDelete = document.createElement("button");
+    elPoroductDelete.className = "btn rounded-0 btn-secondary";
+    const elPoroductDeleteIcon = document.createElement("i");
+    elPoroductDeleteIcon.className = "fa-solid fa-pen";
+    const elPoroductEdit = document.createElement("button");
+    elPoroductEdit.className = "btn rounded-0 btn-danger";
+    const elPoroductEditIcon = document.createElement("i");
+    elPoroductEditIcon.className = "fa-solid fa-trash";
 
-elProductWrapper.append(elProduct);
+
+    elProducts.append(elProductItem);
+  elProductItem.append(elProductImg);
+  elProductItem.append(elPoroductBody);
+  elPoroductBody.append(elProductTitle)
+  elPoroductBody.append(elProductPrice);
+  elProductPrice.append(elProductMark);
+  elPoroductBody.append(elProductModel);
+  elPoroductBody.append(elProductIcons);
+  elProductIcons.append(elPoroductDelete);
+  elPoroductDelete.append(elPoroductDeleteIcon);
+  elProductIcons.append(elPoroductEdit);
+  elPoroductEdit.append(elPoroductEditIcon);
+  
+  elProductWrapper.append(elProducts);
+  };
+
