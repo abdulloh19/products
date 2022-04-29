b07120335969f1c775fb3ee004ec8cb2f76816d7
abdulloh19 <parij4320@gmail.com>
2022-04-29 12:45:14 +0500

    benefits add

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ for (let i = 0; i < products.length; i++) {
 
     const elPoroductBody = createElement("div", "card-body");
 
-    const elProductTitle = createElement("p", "card-title", carrenProduct.title);
+    const elProductTitle = createElement("h3", "card-title", carrenProduct.title);
 
     const elProductPrice = createElement("p", "card-text fw-bold");
 
@@ -34,6 +34,16 @@ for (let i = 0; i < products.length; i++) {
 
     const elProductModel = createElement("p", "badge bg-success", carrenProduct.model);
 
+    const elProductList = createElement("ul", "d-flex flex-wrap list-unstyled");
+    
+    for (let j = 0; j < carrenProduct.benefits.length; j++) {
+
+      const elProductBenefits = createElement("li", "badge bg-primary me-1 mb-1", carrenProduct.benefits[j])
+
+      console.log(elProductBenefits);
+      elProductList.append(elProductBenefits);
+    };
+ 
     const elProductIcons = createElement("div", "position-absolute top-0 end-0 d-flex");
 
     const elPoroductDelete = createElement("button", "btn rounded-0 btn-secondary");
@@ -44,6 +54,8 @@ for (let i = 0; i < products.length; i++) {
     
     const elPoroductEditIcon = createElement("i", "fa-solid fa-trash");
 
+    
+
 
     elProducts.append(elProductItem);
   elProductItem.append(elProductImg);
@@ -53,10 +65,12 @@ for (let i = 0; i < products.length; i++) {
   elProductPrice.append(elProductMark);
   elPoroductBody.append(elProductModel);
   elPoroductBody.append(elProductIcons);
+  elPoroductBody.append(elProductList)
   elProductIcons.append(elPoroductDelete);
   elPoroductDelete.append(elPoroductDeleteIcon);
   elProductIcons.append(elPoroductEdit);
   elPoroductEdit.append(elPoroductEditIcon);
+
   
   elProductWrapper.append(elProducts);
   };
