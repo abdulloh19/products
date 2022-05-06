aa1e877c3ba74703e1e34a8aa2a11a7478b30711
abdulloh19 <parij4320@gmail.com>
2022-05-06 20:06:34 +0500

    product

--- js/main.js
+++ js/main.js
@@ -12,66 +12,62 @@ const createElement = function(tagName, className, text) {
   return createdElement;
 }
 
+const createProduct = function(product) {
+  const productRow = document.createElement("li");
 
-for (let i = 0; i < products.length; i++) {
+  for (let i = 0; i < products.length; i++) {
     const carrenProduct = products[i]
-
     const elProducts = createElement("li", "col-4");
-    
     const elProductItem = createElement("div", "card");
-    
     const elProductImg = createElement("img", "card-img-top");
     elProductImg.src = carrenProduct.img;
-
     const elPoroductBody = createElement("div", "card-body");
-
     const elProductTitle = createElement("h3", "card-title", carrenProduct.title);
-
     const elProductPrice = createElement("p", "card-text fw-bold");
-
     const elProductMark = createElement("mark");
     elProductMark.textContent = carrenProduct.price;
-
     const elProductModel = createElement("p", "badge bg-success", carrenProduct.model);
-
+    const addedDate = new Data (carrenProduct.addedDate)
+    const elProductData = createElement("p", "card-text", "")
     const elProductList = createElement("ul", "d-flex flex-wrap list-unstyled");
-    
+
     for (let j = 0; j < carrenProduct.benefits.length; j++) {
 
       const elProductBenefits = createElement("li", "badge bg-primary me-1 mb-1", carrenProduct.benefits[j])
 
-      console.log(elProductBenefits);
       elProductList.append(elProductBenefits);
     };
- 
+
     const elProductIcons = createElement("div", "position-absolute top-0 end-0 d-flex");
 
     const elPoroductDelete = createElement("button", "btn rounded-0 btn-secondary");
-
     const elPoroductDeleteIcon = createElement("i", "fa-solid fa-pen");
+    elPoroductDelete.append(elPoroductDeleteIcon);
+    elProductIcons.append(elPoroductDelete);
 
     const elPoroductEdit = createElement("button", "btn rounded-0 btn-danger");
-    
     const elPoroductEditIcon = createElement("i", "fa-solid fa-trash");
+    elPoroductEdit.append(elPoroductEditIcon);
+    elProductIcons.append(elPoroductEdit);
 
-    
+  //   elProducts.append(elProductItem);
+  // elProductItem.append(elProductImg);
+  // elProductItem.append(elPoroductBody);
+  // elPoroductBody.append(elProductTitle);
+  // elPoroductBody.append(elProductPrice);
+  // elProductPrice.append(elProductMark);
+  // elPoroductBody.append(elProductModel);
+  // elPoroductBody.append(addedDate);
+  // elPoroductBody.append(elProductData);
+  // elPoroductBody.append(elProductIcons);
+  // elPoroductBody.append(elProductList);
 
-
-    elProducts.append(elProductItem);
-  elProductItem.append(elProductImg);
-  elProductItem.append(elPoroductBody);
-  elPoroductBody.append(elProductTitle)
-  elPoroductBody.append(elProductPrice);
-  elProductPrice.append(elProductMark);
-  elPoroductBody.append(elProductModel);
-  elPoroductBody.append(elProductIcons);
-  elPoroductBody.append(elProductList)
-  elProductIcons.append(elPoroductDelete);
-  elPoroductDelete.append(elPoroductDeleteIcon);
-  elProductIcons.append(elPoroductEdit);
-  elPoroductEdit.append(elPoroductEditIcon);
-
-  
   elProductWrapper.append(elProducts);
+  productRow.append(elProductItem, elProductImg, elPoroductBody, elProductTitle, elProductPrice, elProductMark, elProductModel, elProductData. elProductIcons);
+  
   };
+}
+
+
+
 
